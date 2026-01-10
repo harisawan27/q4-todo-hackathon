9f26929263193109d90cd556a81b39bc6a9ef44c
Muhammad Haris Awan <160159398+harisawan27@users.noreply.github.com>
2026-01-11 01:46:04 +0500

    Enhance auth configuration with SSL and trusted origins
    Added SSL support and trusted origins for authentication.

--- todo-phase-2/frontend/lib/auth.ts
+++ todo-phase-2/frontend/lib/auth.ts
@@ -4,18 +4,29 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
 
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
   database: pool,
+  
+  trustedOrigins: [
+    "https://https://q4-todo-hackathon.vercel.app", // Your production URL
+    "http://localhost:3000",             // Your local development URL
+  ],
+  
   emailAndPassword: {
     enabled: true,
   },
   session: {
     expiresIn: 60 * 60 * 24 * 7, // 7 days
     updateAge: 60 * 60 * 24, // 1 day
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60,
+    },
   },
   user: {
     changeEmail: {
